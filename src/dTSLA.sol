6d385bb9c46b9806d2b85cea3041799e36ebb894
ghaieth96 <ghaiethbmoussa@gmail.com>
2024-07-02 19:19:17 +0100

    feat: mint dTSLA tokens if enough collateral

--- src/dTSLA.sol
+++ src/dTSLA.sol
@@ -17,43 +17,63 @@ contract dTSLA is ERC20, Pausable, ConfirmedOwner, FunctionsClient {
     using OracleLib for AggregatorV3Interface;
 
     error dTSLA__NotEnoughCollateral();
+    error dTSLA__InvalidMintAmount();
 
+    enum MintOrRedeem { mint, redeem }
+
+    struct dTSLARequest {
+        uint256 amountOfToken;
+        address requester;
+        MintOrRedeem mintOrRedeem;
+    }
+
+    bytes32 s_donID;
+    uint64 immutable i_subId;
     string private s_mintSourceCode;
-
-    uint256 s_portfolioBalance;
+    uint32 private constant GAS_LIMIT = 300_000;
 
     address public i_tslaUsdFeed;
+
+    uint256 s_portfolioBalance;
 
     uint256 public constant ADDITIONAL_FEED_PRECISION = 1e10;
     uint256 public constant COLLATERAL_RATIO = 200; // 200% collateral ratio
     uint256 public constant COLLATERAL_PRECISION = 100;
     uint256 private constant PRECISION = 1e18;
 
+    mapping(bytes32 requestId => dTSLARequest request) private s_requestIdToRequest;
 
-    constructor(address functionsRouter, string memory mintSourceCode) 
+    constructor(address functionsRouter, string memory mintSourceCode, bytes32 donId, uint64 subId, address tslaPriceFeed) 
         ERC20("Backed TSLA", "bTSLA") 
         ConfirmedOwner(msg.sender) 
         FunctionsClient(functionsRouter) 
     {
+        s_donID = donId;
+        i_subId = subId;
         s_mintSourceCode = mintSourceCode;
+        i_tslaUsdFeed = tslaPriceFeed;
     }
 
     /// 2 transactions
     /// Sends an HTTP request to:
     /// 1. See how much TSLA is bought
     /// 2. If wnough TSLA is in the Alpaca account, mint dTSLA
-    function sendMintRequest(uint256 amountOfTokensToMint) external onlyOwner whenNotPaused returns(bytes32 requestId) {
-        // they want to mint $100 and the portfolio has $200 - then that's cool
+    function sendMintRequest(uint256 amountOfTokensToMint) external onlyOwner whenNotPaused returns (bytes32 requestId) {
+        if (amountOfTokensToMint != 0) {
+            revert dTSLA__InvalidMintAmount();
+        }
+
+        // If they want to mint $100 and the portfolio has $200, then that's cool
         if (_getCollateralRatioAdjustedTotalBalance(amountOfTokensToMint) > s_portfolioBalance) {
             revert dTSLA__NotEnoughCollateral();
         }
 
         FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(s_mintSourceCode);
+        bytes32 _requestId = _sendRequest(req.encodeCBOR(), i_subId, GAS_LIMIT, s_donID);
+        s_requestIdToRequest[_requestId] = dTSLARequest(amountOfTokensToMint, msg.sender, MintOrRedeem.mint);
 
-    }
-
-    function _mintFulFillRequest() internal {
-
+        return _requestId;
     }
 
     /// @notice User sends a request to sell TSLA for USDC (redemtionToken)
@@ -65,7 +85,40 @@ contract dTSLA is ERC20, Pausable, ConfirmedOwner, FunctionsClient {
 
     }
 
-    function _redeemFulFillRequest() internal {
+    /// @notice Callback function for fulfilling a request
+    /// @param requestId The ID of the request to fulfill
+    /// @param response The HTTP response data
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory /* err */
+    ) internal override {
+        if (s_requestIdToRequest[requestId].mintOrRedeem == MintOrRedeem.mint) {
+            _mintFulFillRequest(requestId,response);
+        } else {
+            _redeemFulFillRequest(requestId,response);
+        }
+    }
+
+    /// Return the amount of TSLA (in USD) is stored in our broker
+    /// If we have enough of TSLA collateral, mint the dTSLA tokens
+    function _mintFulFillRequest(bytes32 requestId, bytes memory response) internal {
+        uint256 amountOfTokensToMint = s_requestIdToRequest[requestId].amountOfToken;
+        s_portfolioBalance = uint256(bytes32(response));
+
+        // if TSLA collateral > dTSLA to min -> mint
+        // 1. How much TSLA in $$$ do we have?
+        // 2. How much TSLA in $$$ are we minting?
+        if (_getCollateralRatioAdjustedTotalBalance(amountOfTokensToMint) > s_portfolioBalance) {
+            revert dTSLA__NotEnoughCollateral();
+        }
+
+        if (amountOfTokensToMint != 0) {
+            _mint(s_requestIdToRequest[requestId].requester, amountOfTokensToMint);
+        }
+    }
+
+    function _redeemFulFillRequest(bytes32 requestId, bytes memory response) internal {
 
     }
     
